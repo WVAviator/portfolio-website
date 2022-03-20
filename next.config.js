8d30c0edb716b3b3bd609bf3619a64e40e93da1d
WVAviator <wvaviator@gmail.com>
2022-03-20 11:09:52 -0500

    add sanity studio

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	reactStrictMode: true,
+const STUDIO_REWRITE = {
+	source: "/studio/:path*",
+	destination:
+		process.env.NODE_ENV === "development"
+			? "http://localhost:3333/studio/:path*"
+			: "/studio/index.html",
 };
 
-module.exports = nextConfig;
+module.exports = {
+	rewrites: () => [STUDIO_REWRITE],
+	reactStrictMode: true,
+};
