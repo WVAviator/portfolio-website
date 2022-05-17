9b3e0b459be47c3035c7b595f53d8497ac538794
Alexander Durham <wvaviator@gmail.com>
2022-05-17 18:59:39 -0500

    Added main portfolio page (#5)
    * added some code for blog posts
    
    * more blog formatting and construction
    
    * more blog formatting and construction
    
    * more blog formatting and construction
    
    * more blog formatting and construction
    
    * more blog formatting and construction
    
    * more blog formatting and construction
    
    * updates to blog section to work with sanity
    
    * configure blog posts to work with sanity
    
    * added smartphone and abstracted out slideshow logic
    
    * added smartphone and abstracted out slideshow logic
    
    * fix props on SanityImage
    
    * added husky and pre-commit hook
    
    * fix lint errors
    
    * update lint-staged
    
    * more attempts to fix husky
    
    * update schemas to include alt text for all image types
    
    * updates to blog styling
    
    * added code entry and syntax highlighting in posts
    
    * added code entry and syntax highlighting in posts
    
    * Adding scrolling tech stack banner to homepage
    
    * complete tech section and fix hero margin
    
    * Added portfolio pages and iframe blog component
    
    * Fix post width with code bloacks and banner overflow
    
    * Added main portfolio page and corrected some sidebar issues for project posts

---
+++ components/layout/ProjectPreview.tsx
@@ -0,0 +1,72 @@
+import Link from "next/link";
+import { Project } from "../../types";
+import Laptop from "../display/Laptop";
+import Smartphone from "../display/Smartphone";
+import SanityImage from "../sanity/SanityImage";
+import Button from "../ui/Button";
+
+interface Props {
+	project: Project;
+}
+
+const ProjectPreview = ({ project }: Props) => {
+	return (
+		<article className="flex flex-wrap gap-10 md:flex-nowrap items-center justify-center shadow-md bg-slate-100 py-6 max-h-[34rem] md:max-h-[19rem]">
+			<div
+				className={
+					"px-6 text-center md:text-left md:w-[50%] flex flex-col gap-6"
+				}
+			>
+				<Link href={`/portfolio/${project.slug.current}`}>
+					<a className="md:max-w-fit">
+						<h2 className="text-xl  hover:text-cyan-400">
+							{project.title}
+						</h2>
+					</a>
+				</Link>
+				<p className="text-sm">{project.description}</p>
+				<div className="flex flex-row gap-6 mx-auto md:mx-0">
+					<Button
+						href={project.projectUrl}
+						className="btn btn-primary"
+					>
+						<span>Published Project</span>
+					</Button>
+					<Button
+						href={project.githubUrl}
+						className="btn btn-primary"
+					>
+						<span>GitHub Repo</span>
+					</Button>
+				</div>
+			</div>
+			<Link href={`/portfolio/${project.slug.current}`}>
+				<a className="group transition-transform hover:scale-105 duration-200">
+					<div className="p-6 lg:p-6 flex flex-col items-center w-[20rem] cursor-pointer  max-h-[13rem] ">
+						{project.desktopView && (
+							<Laptop className="max-w-[100%]">
+								<SanityImage
+									source={project.desktopView}
+									alt={project.title}
+								/>
+							</Laptop>
+						)}
+						{project.mobileView && (
+							<Smartphone className="max-w-[20%] -translate-y-[110%] -translate-x-[180%]">
+								<SanityImage
+									source={project.mobileView}
+									alt={project.title}
+								/>
+							</Smartphone>
+						)}
+					</div>
+					<p className="text-center group-hover:text-cyan-400">
+						Read More
+					</p>
+				</a>
+			</Link>
+		</article>
+	);
+};
+
+export default ProjectPreview;
