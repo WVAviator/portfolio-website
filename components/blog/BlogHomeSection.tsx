416a3a9cbb09392311599d8297d672bc4987fd2c
Wvaviator <wvaviator@gmail.com>
2023-01-23 15:38:30 -0600

    fix up styling on blog home page

--- components/blog/BlogHomeSection.tsx
+++ components/blog/BlogHomeSection.tsx
@@ -10,9 +10,12 @@ interface BlogHomeSectionProps {
 const BlogHomeSection: React.FC<BlogHomeSectionProps> = ({ posts }) => {
   return (
     <section className="py-4">
-      <ul className="flex gap-2">
+      <ul className="flex flex-wrap justify-center lg:justify-start gap-3">
         {posts.map((post) => (
-          <li key={post.slug.current}>
+          <li
+            key={post.slug.current}
+            className="flex justify-center items-center"
+          >
             <PostCard post={post} />
           </li>
         ))}
--- components/blog/PostCard.tsx
+++ components/blog/PostCard.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link';
 import { BlogPost } from '../../types';
 import SanityImage from '../sanity/SanityImage';
 
+const DESCRIPTION_CHARACTER_LIMIT = 180;
+
 interface PostCardProps {
   post: BlogPost;
 }
@@ -9,12 +11,23 @@ interface PostCardProps {
 const PostCard: React.FC<PostCardProps> = ({ post }) => {
   const formattedUpdateAt = new Date(post._updatedAt).toLocaleDateString();
 
+  let truncatedDescription = post.description;
+  if (post.description.length > DESCRIPTION_CHARACTER_LIMIT) {
+    // set the truncated description to the first 100 characters but also don't cut off the middle of a word
+    truncatedDescription =
+      post.description
+        .slice(0, DESCRIPTION_CHARACTER_LIMIT)
+        .split(' ')
+        .slice(0, -1)
+        .join(' ') + '...';
+  }
+
   return (
     <Link
       aria-describedby={`description-${post.slug.current}`}
       aria-labelledby={`title-${post.slug.current}`}
       href={`/blog/${post.slug.current}`}
-      className="flex flex-col aspect-[0.85] max-w-[21rem] p-4 border rounded shadow-md bg-primary-50  hover:shadow-lg transition-shadow duration-200"
+      className="flex flex-col aspect-[0.85] min-w-[15rem] w-[90%] max-w-[21rem] sm:w-[21rem] xsm:h-[30rem] p-4 border rounded shadow-md bg-primary-50  hover:shadow-lg transition-shadow duration-200"
     >
       <div className="relative  aspect-[16/10] shadow-inner border rounded overflow-hidden">
         <SanityImage
@@ -28,7 +41,7 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
 
       <h2
         id={`title-${post.slug.current}`}
-        className="text-md text-center font-semibold xsm:text-left xsm:text-lg"
+        className="text-md text-center font-semibold xsm:text-left xsm:text-lg mt-4"
       >
         {post.title}
       </h2>
@@ -37,7 +50,7 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
         id={`description-${post.slug.current}`}
         className="text-center text-sm xsm:text-left xsm:text-base my-3"
       >
-        {post.description}
+        {truncatedDescription}
       </p>
       <p className="mt-auto text-center text-xs xsm:text-left xsm:text-sm">
         Last updated {formattedUpdateAt}
