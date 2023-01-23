0864f8eb02d4885cbc6e60a1b3d6a670b4356a55
Wvaviator <wvaviator@gmail.com>
2023-01-23 15:55:49 -0600

    add page loading transitions

---
+++ components/ui/PageLoading.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import InitialsLoadingIndicator from '../brand/InitialsLoadingIndicator';
+
+const PageLoading = () => {
+  return (
+    <div className="h-[50vh] w-screen">
+      <InitialsLoadingIndicator />
+    </div>
+  );
+};
+
+export default PageLoading;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import PageLayout from '../components/layout/PageLayout';
+import usePageFallback from '../lib/hooks/usePageFallback';
+import PageLoading from '../components/ui/PageLoading';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<PageLayout>
-			<Component {...pageProps} />
-		</PageLayout>
-	);
+  const { loading } = usePageFallback();
+  return (
+    <PageLayout>
+      {loading ? <PageLoading /> : <Component {...pageProps} />}
+    </PageLayout>
+  );
 }
 
 export default MyApp;
