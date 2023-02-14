ae99a68c4dc463e62fbed1aaf69090673c422d0d
Alexander Durham <wvaviator@gmail.com>
2023-02-14 09:44:52 -0600

    update revlaidation logic

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,46 +1,68 @@
 import { Technology } from './../../types.d';
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Method not allowed' });
-  }
+	if (req.method !== 'POST') {
+		return res.status(405).json({
+			message: 'Method not allowed',
+			request: {
+				method: req.method,
+				body: req.body,
+			},
+		});
+	}
 
-  if (req.headers.token !== process.env.REVALIDATE_TOKEN) {
-    return res.status(401).json({ message: 'Invalid token' });
-  }
+	if (req.headers.token !== process.env.REVALIDATE_TOKEN) {
+		return res.status(401).json({
+			message: 'Invalid token',
+			request: {
+				method: req.method,
+				body: req.body,
+			},
+		});
+	}
 
-  try {
-    const typeTranslation = {
-      post: 'blog',
-      technology: 'technology',
-      project: 'portfolio',
-    };
+	try {
+		const typeTranslation = {
+			post: 'blog',
+			technology: 'technology',
+			project: 'portfolio',
+		};
 
-    console.log('Received revalidation request with body:', req.body);
+		console.log('Received revalidation request with body:', req.body);
 
-    const { slug, type } = req.body as {
-      slug: {
-        current: string;
-      };
-      type: keyof typeof typeTranslation;
-    };
+		const { slug, type } = req.body as {
+			slug: {
+				current: string;
+			};
+			type: keyof typeof typeTranslation;
+		};
 
-    if (!(type in typeTranslation)) {
-      return res.status(400).json({ message: 'Invalid type' });
-    }
+		if (!(type in typeTranslation)) {
+			return res.status(400).json({
+				message: 'Invalid type',
+				type,
+				slug,
+				request: {
+					method: req.method,
+					body: req.body,
+				},
+			});
+		}
 
-    const revalidationUrl = `/${typeTranslation[type]}/${slug.current}`;
-    console.log('Revalidating', revalidationUrl);
+		const revalidationUrl = `/${typeTranslation[type]}/${slug.current}`;
+		console.log('Revalidating', revalidationUrl);
 
-    await res.revalidate(revalidationUrl);
-    return res.json({ message: 'Successfully revalidated!' });
-  } catch (err) {
-    return res
-      .status(500)
-      .send(`Error occurred in revalidation process: ${err}`);
-  }
+		await res.revalidate(revalidationUrl);
+		return res.json({
+			message: 'Successfully revalidated!',
+			revalidated: true,
+		});
+	} catch (err) {
+		return res
+			.status(500)
+			.send(`Error occurred in revalidation process: ${err}`);
+	}
 };
 
 export default handler;
