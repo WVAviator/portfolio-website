8120ffae01630f02fbe2aca82648adac1fc870c5
Alexander Durham <wvaviator@gmail.com>
2023-10-26 09:43:12 -0500

    update fallback for paths to blocking

--- pages/blog/[post].tsx
+++ pages/blog/[post].tsx
@@ -67,7 +67,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false,
+    fallback: 'blocking',
   };
 };
 
--- pages/portfolio/[project].tsx
+++ pages/portfolio/[project].tsx
@@ -75,7 +75,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false,
+    fallback: 'blocking',
   };
 };
 
